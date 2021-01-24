3916ad4349460b514b32c206541c4a5981412ee1
Yurii Chmutov <vad4807@gmail.com>
2021-01-24 19:59:16 +0200

    Delete Timer.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from scipy.spatial import distance
+import time
 
 img_lester = cv2.imread("Images/Lester512.png")
 img_liverpool = cv2.imread("Images/Liverpool.jpg")
@@ -11,6 +12,10 @@ orb = cv2.ORB_create(nfeatures=500)
 keypoints_lester, descriptors_lester = orb.detectAndCompute(img_lester, None)
 keypoints_liverpool, descriptors_liverpool = orb.detectAndCompute(img_liverpool, None)
 keypoints_liverpool_side, descriptors_liverpool_side = orb.detectAndCompute(img_liverpool_side, None)
+
+# img = cv2.drawKeypoints(img_liverpool, keypoints_liverpool, None)
+# cv2.imshow("Liverpool", img)
+# cv2.waitKey(0)
 
 
 # to convert number to bit array 8 -> [0, 0, 0, 0, 1, 0, 0, 0]
@@ -60,8 +65,6 @@ descriptors_lester_bit_format = convert_32_descriptors_to_256_bit(descriptors_le
 
 # print(descriptors_liverpool_side_bit_format)
 
-first_class: int = 0
-second_class: int = 0
 
 etalon_liverpool_plus_lester_bit_format = descriptors_liverpool_bit_format + descriptors_lester_bit_format
 
@@ -77,23 +80,32 @@ def findMinimalHammingDistanceForDescriptor(descriptor, etalons):
             index_of_min_distance = index
             min_distance = current_distance
         index += 1
-    # print(f'Index of min: {index_of_min_distance}')
-    # print(f'Min distance: {min_distance}')
-    # print(f'index: {index_of_min_distance}')
     return index_of_min_distance
 
 
 # print(findMinimalHammingDistanceForDescriptor(li))
 
+def main():
+    start_time = time.time()
+    first_class: int = 0
+    second_class: int = 0
+    for i in range(0, 500):
+        index_from_etalon = \
+            findMinimalHammingDistanceForDescriptor(descriptors_liverpool_side_bit_format[i],
+                                                    etalon_liverpool_plus_lester_bit_format)
+        if index_from_etalon < 500:
+            first_class += 1
+        else:
+            second_class += 1
 
-for i in range(0, 500):
-    index_from_etalon = \
-        findMinimalHammingDistanceForDescriptor(descriptors_liverpool_side_bit_format[i],
-                                                etalon_liverpool_plus_lester_bit_format)
-    if index_from_etalon < 500:
-        first_class += 1
-    else:
-        second_class += 1
+    print(f'First class: {first_class}')
+    print(f'Second class: {second_class}')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
-print(f'First class: {first_class}')
-print(f'Second class: {second_class}')
+
+if __name__ == "__main__":
+    main()
+
+
+
+
